e518017b18a749198f1b67f6cf72b14798a36faf
kondratvlad <vladkondrat87@gmail.com>
2024-10-31 12:40:39 +0200

    first master commit

---
+++ UnitTest3/UnitTest3.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector>
+#include <cmath>
+#include "../lab 5.05.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest3
+{
+    TEST_CLASS(UnitTest3)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            // Arrange
+            int a = 2, b = 2, c = 1, m = 10;
+            int V1 = 1, V2 = 2, n = 5;
+            std::vector<int> expected = { 1, 2, 5, 6, 7 }; // Expected sequence
+
+            // Act
+            std::vector<int> result = pseudorandom_generator(a, b, c, m, V1, V2, n);
+
+            // Assert
+            Assert::AreEqual(expected.size(), result.size(), L"Sequence sizes do not match.");
+            for (size_t i = 0; i < expected.size(); ++i) {
+                Assert::AreEqual(expected[i], result[i], L"Sequence values do not match at index " + std::to_wstring(i));
+
+
+            }
+        };
+    };
---
+++ lab 5.05.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath> 
+
+using namespace std;
+
+vector<int> pseudorandom_generator(int a, int b, int c, int m, int V1, int V2, int n) {
+    vector<int> sequence;
+    sequence.push_back(V1);
+    sequence.push_back(V2);
+    for (int i = 2; i < n; ++i) {
+        int Vi = (a * static_cast<int>(pow(sequence[i - 1], b)) + c) % m;
+        sequence.push_back(Vi);
+    }
+    return sequence;
+}
+
+int main() {
+    srand(time(0));
+
+    // Параметри
+    int a = 5;
+    int b = 3;
+    int c = 7;
+    int m = 100;
+    int V1 = rand() % m;
+    int V2 = rand() % m;
+    int n = 10;  // Кількість чисел у послідовності
+
+    // Генерація послідовності
+   vector<int> sequence = pseudorandom_generator(a, b, c, m, V1, V2, n);
+
+    // Виведення послідовності
+    for (int num : sequence) {
+       cout << num << " ";
+    }
+   cout << endl;
+
+    return 0;
+}
